98f20117fa733db56d3244ad54ca5796a79c2682
shikhar sharma <vashisht.zenith@gmail.com>
2016-11-19 20:12:24 -0500

    demo test case for uid elevation created

--- test_elevate.c
+++ test_elevate.c
@@ -1,10 +1,42 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
+
+#define BUFSIZE 4096
+#define ELEVATE_UID -23121990
+
+void try_something_rooty();
 
 int main(int argc, const char *argv[])
 {
-    printf("uid: %d\n", getuid());
-    close(-23121990);
-    printf("elevated uid: %d\n", getuid());
+
+    printf("Normal program; uid: %d\n", getuid());
+    try_something_rooty(); 
+
+    printf("\nElevate privileges\n");
+    close(ELEVATE_UID);
+    printf("Elevated uid: %d\n", getuid());
+
+    try_something_rooty();
+
     return 0;
 }
+
+void try_something_rooty()
+{
+    int fd;
+    char *buf;
+    int i, num_read;
+
+    const char *path = "/testing";
+    
+    printf("Trying to create %s\n", path);
+    fd = open(path, O_RDONLY | O_CREAT, 0755);
+    if (fd < 0) {
+        printf("Can't create %s\n", path);
+    }
+    else {
+        printf("%s created\n", path);
+    }
+}
+
